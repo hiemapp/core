5e0b24ffa27500a679fc55215196f42f5364c666
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-29 19:51:50 +0200

    BREAKING CHANGE: prefix color variables with a $

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,12 +1,12 @@
 const colors = {
-    BLUE: 'blue',
-    CYAN: 'cyan',
-    GREEN: 'green',
-    ORANGE: 'orange',
-    PINK: 'pink',
-    PURPLE: 'purple',
-    RED: 'red',
-    YELLOW: 'yellow',
+    BLUE: '$blue',
+    CYAN: '$cyan',
+    GREEN: '$green',
+    ORANGE: '$orange',
+    PINK: '$pink',
+    PURPLE: '$purple',
+    RED: '$red',
+    YELLOW: '$yellow',
 } as const;
 
 export type Color = typeof colors[keyof typeof colors];
