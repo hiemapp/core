6ccc63d8fdb1f175bd43bfe34716d7ffd6b5f256
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-29 22:24:53 +0200

    feat(types): add ChildNode type

--- src/dashboard/DashboardWidget/utils/types.ts
+++ src/dashboard/DashboardWidget/utils/types.ts
@@ -1,5 +1,6 @@
-import WidgetNode from '../nodes/WidgetNode';
-import TextNode from '../nodes/TextNode';
+import type WidgetNode from '../nodes/WidgetNode';
+import type TextNode from '../nodes/TextNode';
 
 export type ValidNode = WidgetNode | TextNode;
 export type Node = ValidNode | null;
+export type ChildNode = Node | string;
