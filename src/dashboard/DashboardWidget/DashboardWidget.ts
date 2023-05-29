86ca1c536f9f4f0e335da88b5bc077c3d3d55b85
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-29 22:24:21 +0200

    feat: add state support feat: add sessionId property

--- src/dashboard/DashboardWidget/DashboardWidget.ts
+++ src/dashboard/DashboardWidget/DashboardWidget.ts
@@ -3,10 +3,16 @@ import icons from '~/utils/icons';
 import { DashboardWidgetManifest } from '~types';
 import { DashboardWidgetNode } from '~/extensions/api/dashboard';
 import colors from '~/utils/colors';
+import { WebSocket } from '~/lib';
 
-export default class DashboardWidget extends ExtensionModule {
-    constructor() {
+export default class DashboardWidget<TState extends Record<string, any> = {}> extends ExtensionModule {
+    #state: TState = {} as TState;
+    sessionId: string;
+
+    constructor(sessionId: string) {
         super();
+        
+        this.sessionId = sessionId;
     }
 
     getManifest(): DashboardWidgetManifest {
@@ -20,8 +26,26 @@ export default class DashboardWidget extends ExtensionModule {
     onMount(): void {
 
     }
-    
+     
     render(): DashboardWidgetNode {
         return null;
+    }
+
+    getState<TKey extends keyof TState>(key: TKey): TState[TKey] {
+        return this.#state[key];
+    }
+
+    setState<TKey extends keyof TState>(key: TKey, value: TState[TKey]): void {
+        const currentValue = this.#state[key];
+        if(value !== currentValue) {
+            this.#state[key] = value;
+            this.#emitUpdateEvent();
+        }   
+    }
+
+    #emitUpdateEvent() {
+        WebSocket.emit('dashboard:widgetupdate', {
+            widgetSessionId: this.sessionId
+        })
     }
 }
