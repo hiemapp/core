5301eae712b7a3198337073020dff7b32c526f5b
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-05-29 21:02:50 +0200

    fix: allow render() method to return a string

--- src/extensions/api/dashboard.ts
+++ src/extensions/api/dashboard.ts
@@ -1,4 +1,4 @@
 export * from '~/dashboard';
 export * as components from '~/dashboard/DashboardWidget/components/index';
-export { Node as DashboardWidgetNode } from '~/dashboard/DashboardWidget/utils/types';
+export { ChildNode as DashboardWidgetNode } from '~/dashboard/DashboardWidget/utils/types';
 export { DashboardWidgetManifest } from '~types';
