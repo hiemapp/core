bf51801b3b0fd6f8c6c5dca7a1bb4a0003549027
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-01 07:27:42 +0200

    feat: add taskrunner util export

--- src/extensions/api/utils.ts
+++ src/extensions/api/utils.ts
@@ -1,3 +1,4 @@
 export { logger } from '../../lib/Logger';
 export * as style from '../../utils/style';
 export { default as icons } from '../../utils/icons';
+export { default as taskrunner } from '../../lib/Taskrunner';
