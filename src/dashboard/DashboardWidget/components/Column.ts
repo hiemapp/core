8c1ad990f427aac05e13d4cb0e35a1b8af8c2b02
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-01 07:13:24 +0200

    feat: add Column component

---
+++ src/dashboard/DashboardWidget/components/Column.ts
@@ -0,0 +1,46 @@
+import { componentFactory } from '../utils/helpers';
+import WidgetNode from '../nodes/WidgetNode';
+
+type ColumnSpan = 1|2|3|4|5|6|7|8|9|10|11|12|'auto';
+type ColumnBreakpoint = 'default'|'xs'|'sm'|'md'|'lg'|'xl';
+export interface ColumnProps {
+    span?: ColumnSpan | Partial<Record<ColumnBreakpoint, ColumnSpan>>
+}
+
+function getColClasses(span: Required<ColumnProps>['span']) {
+    if(!span) {
+        return [];
+    }
+
+    if(typeof span === 'number') {
+        span = { default: span };
+    }
+
+    let classList: string[] = [];
+    Object.entries(span).forEach(([ breakpoint, breakpointSpan ]) => {
+        let className = `col-${breakpoint}-${breakpointSpan}`;
+
+        if(breakpoint === 'default') {
+            className = `col-${breakpoint}`;
+        }
+
+        classList.push(className);
+    })
+
+    return classList;
+}
+
+const Column = componentFactory<ColumnProps>(
+    ({ span, className, children, ...props }) => {
+        return new WidgetNode(
+            'div', 
+            {
+                ...props,
+                className: [className, span && getColClasses(span)]
+            },
+            children
+        );
+    }
+)
+
+export default Column;
