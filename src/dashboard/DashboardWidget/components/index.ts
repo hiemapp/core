0cf835a6340bc82590cdd7062ca7c4a15f2d8d9b
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-01 07:18:09 +0200

    refactor: add exports for the new components

--- src/dashboard/DashboardWidget/components/index.ts
+++ src/dashboard/DashboardWidget/components/index.ts
@@ -1,4 +1,7 @@
-export { default as Header } from './Header';
+export { default as Box } from './Box';
 export { default as Icon } from './Icon';
 export { default as Button } from './Button';
 export { default as Fragment } from './Fragment';
+export { default as Tile } from './Tile';
+export { default as Column } from './Column';
+export { default as span } from './span';
