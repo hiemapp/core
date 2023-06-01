5468ac958873b0e08b3676bedc9445d9e68a7c31
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-01 07:16:05 +0200

    refactor: add DashboardWidgetManager export

--- src/dashboard/index.ts
+++ src/dashboard/index.ts
@@ -1,2 +1,3 @@
 export { default as DashboardWidget } from './DashboardWidget/DashboardWidget';
+export { default as DashboardWidgetManager } from './DashboardWidgetManager';
 export { renderWidgetAndSerialize } from './DashboardWidget/utils/helpers';
