127e1e0da5520e944be4760a9d4a6cfd788a2db6
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-01 09:58:22 +0200

    fix: allow gutter props to be 0

--- src/dashboard/DashboardWidget/components/Box.ts
+++ src/dashboard/DashboardWidget/components/Box.ts
@@ -3,8 +3,8 @@ import WidgetNode from '../nodes/WidgetNode';
 
 export interface BoxProps {
     direction?: 'row' | 'column';
-    gutterX?: 1 | 2 | 3 | 4 | 5;
-    gutterY?: 1 | 2 | 3 | 4 | 5;
+    gutterX?: 0 | 1 | 2 | 3 | 4 | 5;
+    gutterY?: 0 | 1 | 2 | 3 | 4 | 5;
     align?: 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'self-start' | 'self-end',
     justify?: 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'self-start' | 'self-end' | 'space-between' | 'space-around' | 'space-evenly' | 'stretch'
     wrap?: 'wrap' | 'nowrap'
