408d0aa48ee94fc6932bd26f85d8477bfb50bd57
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-03 22:19:41 +0200

    feat: add back event listener support

--- src/ui/jsx.ts
+++ src/ui/jsx.ts
@@ -1,5 +1,5 @@
 import HTMLElement from './HTMLElement';
-import type { HTMLElementProps, ValidElement, AnyElement } from '~types';
+import type { HTMLElementProps, HTMLElementListener, ValidElement, AnyElement } from '~types';
 import { formatElement } from './utils/helpers';
 import { forOwn } from 'lodash';
 
@@ -15,15 +15,21 @@ export function h<TProps extends HTMLElementProps>(type: ((...args: any[]) => An
     }
 
     const props: HTMLElementProps = {};
+    const listeners: Record<string, HTMLElementListener> = {};
     forOwn(config, (value, key) => {
-        if(key.toLowerCase().startsWith('on')) {
-            // Event listener
+        if(typeof key !== 'string') {
+            return true;
+        }
+
+        if(key.trim().toLowerCase().startsWith('on')) {
+            listeners[key] = value as HTMLElementListener;
             return true;
         }
         
         (props as any)[key] = value;
     })
     
+    // Return a plain HTML element
     if(typeof type === 'string') {
         // @ts-ignore
         if(type === 'clientelement' && typeof props?.tag === 'string') {
@@ -31,11 +37,12 @@ export function h<TProps extends HTMLElementProps>(type: ((...args: any[]) => An
             return h(props.tag, props.props, ...(children ?? []));
         }
 
-        return new HTMLElement(type, props, children);
+        return new HTMLElement(type, props, children, listeners);
     }
 
+    // Call the component render function
     if(typeof type === 'function') {
-        const element = type({ ...props, children });
+        const element = type({ ...props, ...listeners, children });
         return formatElement(element);
     }
 
