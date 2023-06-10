3e3f32bed639d2080321dcf3bbf50feec2dedef0
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-06-10 16:03:16 +0200

    feat: add getBlocksOfType method

--- src/flows/FlowController.ts
+++ src/flows/FlowController.ts
@@ -1,10 +1,25 @@
+import { Constructor } from '~types/helpers';
 import ControllerDatabase from '../lib/ControllerDatabase';
 import Flow from './Flow';
+import FlowBlock from './FlowBlock';
+import FlowBlockContext from './FlowBlockContext/FlowBlockContext';
 
 export default class FlowController extends ControllerDatabase<Flow>() {
     static table = 'flows';
 
     static load() {
         return super.load(Flow);
+    }
+
+    static getBlocksOfType(type: Constructor<FlowBlock>) {
+        const blocks: FlowBlockContext[] = [];
+
+        this.index().forEach(flow => {
+            flow.getBlocks().forEach(block => {
+                blocks.push(block);
+            })
+        })
+
+        return blocks;
     }
 }
