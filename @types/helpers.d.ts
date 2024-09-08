6f7591a1a5a24e17fba807c3b94cdc58b159b07a
Tjalling Feenstra <hello@tjalling.net>
2024-09-08 23:24:07 +0200

    chore: remove unused import

--- @types/helpers.d.ts
+++ @types/helpers.d.ts
@@ -1,5 +1,4 @@
 import type ModelWithProps from '~/lib/ModelWithProps';
-import type { ExtensionModule } from '~/extensions/ExtensionModule';
 export type Constructor<T> = new (...args: any[]) => T;
 export type GetPropsSerializedType<M extends ModelWithProps<any>> = M extends ModelWithProps<infer T> ? T['serializedProps'] : never;
 export type Values<T extends Array> = T[number];
