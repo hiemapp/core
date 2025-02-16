05b9fdc7f87050e4a3d55d85daad7a7bbce50a88
Tjalling Feenstra <hello@tjalling.net>
2025-02-16 19:38:57 +0100

    update: add fieldTypes enum

--- src/records/index.ts
+++ src/records/index.ts
@@ -2,3 +2,4 @@ export { default as ImmutableRecord } from './ImmutableRecord';
 export { default as MutableRecord } from './MutableRecord';
 export { default as RecordManager } from './RecordManager';
 export { default as RecordSampler } from './RecordSampler';
+export * as fieldTypes from './fieldTypes';
