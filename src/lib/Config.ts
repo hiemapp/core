b7613f9e5e6fc86b651db69836c5991d26f32aab
Tjalling Feenstra <hello@tjalling.net>
2025-03-01 16:36:55 +0100

    feat: split default and user config files

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -4,9 +4,10 @@ import _ from 'lodash';
 import { dirs } from '../utils/paths';
 import { logger } from '../lib/Logger';
 import { glob } from 'glob';
+import jsonfile from 'jsonfile';
 
 export default class Config {
-    private static cache: {[key: string]: any} = {};
+    private static cache: { [key: string]: any } = {};
     private static rootDir: string;
 
     static getRootDir() {
@@ -15,22 +16,26 @@ export default class Config {
 
     static async load(rootDir: string) {
         this.rootDir = rootDir;
-        const pattern = path.join(dirs().CONFIG, '*.json').replace(/\\/g, '/');
-        const filepaths = await glob(pattern, { absolute: true });
-        
-        for(const filepath of filepaths) {
+
+        // Get all the default and user file paths
+        const defaultFilepaths = await glob('default/*.json', { absolute: true, cwd: dirs().CONFIG });
+        const userFilepaths = await glob('user/*.json', { absolute: true, cwd: dirs().CONFIG });
+
+        // Read all default config files
+        await Promise.all(defaultFilepaths.map(async (filepath) => {
+            const filename = path.parse(filepath).name;
+            this.cache[filename] = await jsonfile.readFile(filepath);
+        }));
+
+        // Read all user config files and replace
+        await Promise.all(userFilepaths.map(async (filepath) => {
             const filename = path.parse(filepath).name;
 
-            try {
-                const contents = JSON.parse(await fs.readFile(filepath, 'utf8'));
-                this.cache[filename] = contents;
-            } catch(err: any) {
-                logger.error(`The following error occured while reading config file '${filename}':`);
-                logger.error(err);
-            } 
-        }
+            const contents = await jsonfile.readFile(filepath);
+            this.cache[filename] = _.merge(this.cache[filename] ?? {}, contents);
+        }));
 
-        return this.cache;
+        console.log(this.get('home'));
     }
 
     static get(keypath: string): any {
@@ -49,38 +54,38 @@ export default class Config {
 
     static getOrCreate(keypath: string, newValue: any): any {
         const currentValue = this.getOrFail(keypath);
-        if(typeof currentValue !== 'undefined') {
+        if (typeof currentValue !== 'undefined') {
             return currentValue;
         }
 
         this.update(keypath, newValue);
 
-        if(this.getOrFail(keypath) === newValue) {
+        if (this.getOrFail(keypath) === newValue) {
             logger.debug(`Created new config entry '${keypath}'.`);
         } else {
-            throw new Error(`Failed to create new config entry '${keypath}'.`)
+            throw new Error(`Failed to create new config entry '${keypath}'.`);
         }
-        
+
         return newValue;
     }
 
     static update(keypath: string, value: any) {
-        const [ filename ] = this.splitKeypath(keypath);
-        if(typeof this.cache[filename] === 'undefined') {
+        const [filename] = this.splitKeypath(keypath);
+        if (typeof this.cache[filename] === 'undefined') {
             return;
         }
 
         // Update the cache
         _.set(this.cache, keypath, value);
-        
-        const filepath = path.join(dirs().CONFIG, filename+'.json');
-        
+
+        const filepath = path.join(dirs().CONFIG, filename + '.json');
+
         // Write the updated data to the file
         return fs.writeFile(filepath, JSON.stringify(this.cache[filename]));
     }
-    
-    private static splitKeypath(keypath: string): [ string, string ] {
+
+    private static splitKeypath(keypath: string): [string, string] {
         const split = keypath.split('.');
-        return [ split[0], split.slice(1).join('.') ];
+        return [split[0], split.slice(1).join('.')];
     }
 }
